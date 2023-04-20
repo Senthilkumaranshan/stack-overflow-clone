bb673f5810d8b12750d769fbbf23ac7551bb712e
Senthilkumaranshan <skdudeflow@gmail.com>
2023-04-20 19:50:13 +0530

    Sync initial project setup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app id="Footer"> 
+    <Header />
+    <v-container fluid>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
